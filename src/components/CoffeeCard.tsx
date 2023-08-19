e40d9f2147e5df8d3ef2ce6318147f44921958a3
xninjaxbr <81336328+xninjaxbr@users.noreply.github.com>
2023-08-18 23:35:58 -0300

    Finalizado

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -1,36 +1,56 @@
 import { Minus, Plus, ShoppingCart } from '@phosphor-icons/react'
 import { Card, Coin, Amount } from './style/CoffeeCard.style'
+import { useContext, useState } from 'react'
+import { CartContext } from './context/CartContext'
+import { setPreice } from './GlobalFunctions'
 
 interface Icardprops {
+  key?: number
+  id: number
   imgCoffee: string
-  tagCoffee: string
+  tagCoffee: string[]
   titleCoffee: string
   discriptionCoffee: string
-  priceCoffee: string
+  priceCoffee: number
 }
 
 export function CoffeeCard({
+  id,
   imgCoffee,
   tagCoffee,
   discriptionCoffee,
   priceCoffee,
   titleCoffee,
 }: Icardprops) {
-  function setPreice(price: string) {
-    let tmpPrice = price + ''
-    tmpPrice = tmpPrice.replace(/([0-9]{2})$/g, ',$1')
-    if (tmpPrice.length > 6)
-      tmpPrice = tmpPrice.replace(/([0-9]{3}),([0-9]{2}$)/g, '.$1,$2')
-    return tmpPrice
+  const cartContext = useContext(CartContext)
+
+  function addToCart() {
+    cartContext.addCart(id, amount, priceCoffee)
   }
 
+  let [amount, setAmount] = useState(1)
+
+  function plusAmount() {
+    setAmount((amount += 1))
+  }
+  function minusAmount() {
+    if (amount > 1) {
+      setAmount((amount -= 1))
+    }
+  }
+
+  console.log(cartContext.cart.totalprice)
   return (
     <Card>
       <img src={imgCoffee} alt={`Imagem do café ${discriptionCoffee}`} />
       <div>
-        <span className="tag">
-          <p>{tagCoffee}</p>
-        </span>
+        <div className="tags">
+          {tagCoffee.map((tag) => (
+            <span key={tag} className="tag">
+              {tag}
+            </span>
+          ))}
+        </div>
         <div className="title">
           <h1>{titleCoffee}</h1>
           <h2>{discriptionCoffee}</h2>
@@ -38,18 +58,18 @@ export function CoffeeCard({
         <div className="cart">
           <Coin>
             <span className="coin">R$</span>
-            <p>{setPreice(priceCoffee.toString())}</p>
+            <p>{setPreice(priceCoffee, false)}</p>
           </Coin>
           <Amount>
-            <button>
+            <button onClick={minusAmount}>
               <Minus size={14} />
             </button>
-            <span>1</span>
-            <button>
+            <span>{amount}</span>
+            <button onClick={plusAmount}>
               <Plus size={14} />
             </button>
           </Amount>
-          <button>
+          <button onClick={addToCart}>
             <ShoppingCart size={22} color="#F3F2F2" weight="fill" />
           </button>
         </div>
